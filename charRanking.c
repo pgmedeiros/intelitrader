c6a916e56b6676ad917fce8e2926ff590ac7f8c5
medeiros <emaildopedromedeiros@gmail.com>
2023-09-13 16:46:39 -0300

    char_ranking adiciona var size e reafatora nome insert

--- charRanking.c
+++ charRanking.c
@@ -14,9 +14,11 @@ struct node {
 	
 };
 
+int size = 0;
+
 Node * getNode() {
 	Node * node = (Node *) malloc(sizeof(Node));
-	node->value = 0;
+	node->value = 1;
 	node->left = NULL;
 	node->right = NULL;
 	node->key = NULL;
@@ -131,19 +133,23 @@ int calculateBFactor(Node * root) {
 }
 
 
-Node * inserir (Node * root, Node * pai, Node * node, int d) {
+Node * insertOrUpdate (Node * root, Node * pai, Node * node, int d) {
 	
 	Node * retorno;
 	int direction;
 	
+	if (node->key == root->key) {
+		root->value++;
+	}
+	
 	if (node->key > root->key && root->right != NULL) {
 		direction = 1;
-		retorno = inserir(root->right, root, node, direction);
+		retorno = insertOrUpdate(root->right, root, node, direction);
 	}
 	
 	if (node->key < root->key && root->left != NULL) {
 		direction = -1;
-		retorno = inserir(root->left, root, node, direction);
+		retorno = insertOrUpdate(root->left, root, node, direction);
 	}
 	
 	if (node->key > root->key && root->right == NULL) {
@@ -151,6 +157,7 @@ Node * inserir (Node * root, Node * pai, Node * node, int d) {
 		if (root->left == NULL) {
 			root->height += 1;
 		}
+		size++;
 	}
 	
 	if (node->key < root->key && root->left == NULL) {
@@ -158,6 +165,7 @@ Node * inserir (Node * root, Node * pai, Node * node, int d) {
 		if (root->right == NULL) {
 			root->height += 1;
 		}
+		size++;
 	}
 	
 	if (root->left != NULL || root->right != NULL) {
@@ -188,6 +196,8 @@ Node * inserir (Node * root, Node * pai, Node * node, int d) {
 	
 	return root;
 }
+
+
 
 
 int main( void ) {
@@ -226,19 +236,19 @@ int main( void ) {
 	Node * nodeToInsert10 = getNode();
 	nodeToInsert10->key = 23;
 	
-	root = inserir(root, NULL, nodeToInsert, 0);
-	root = inserir(root, NULL, nodeToInsert2, 0);
-	root = inserir(root, NULL, nodeToInsert3, 0);
-	root = inserir(root, NULL, nodeToInsert4, 0);
-	root = inserir(root, NULL, nodeToInsert5, 0);
-	root = inserir(root, NULL, nodeToInsert6, 0);
-	root = inserir(root, NULL, nodeToInsert7, 0);
-	root = inserir(root, NULL, nodeToInsert8, 0);
-	root = inserir(root, NULL, nodeToInsert9, 0);
-	root = inserir(root, NULL, nodeToInsert10, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert2, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert3, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert4, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert5, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert6, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert7, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert8, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert8, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert9, 0);
+	root = insertOrUpdate(root, NULL, nodeToInsert10, 0);
 	
 	printf("||||||||||||||||||||||||||||||");
-
 
 	printf("\na root %i, valor: %i", root->key == 15, root->key);
 		printf("\nbroot %i, valor: %i", root->left->key == 2, root->left->key);
@@ -273,11 +283,11 @@ int main( void ) {
 	Node * _2nodeToInsert5 = getNode();
 	_2nodeToInsert5->key = 58;
 	
-	root2 = inserir(root2, NULL, _2nodeToInsert, 0);
-	root2 = inserir(root2, NULL, _2nodeToInsert2, 0);
-	root2 = inserir(root2, NULL, _2nodeToInsert3, 0);
-	root2 = inserir(root2, NULL, _2nodeToInsert4, 0);
-	root2 = inserir(root2, NULL, _2nodeToInsert5, 0);
+	root2 = insertOrUpdate(root2, NULL, _2nodeToInsert, 0);
+	root2 = insertOrUpdate(root2, NULL, _2nodeToInsert2, 0);
+	root2 = insertOrUpdate(root2, NULL, _2nodeToInsert3, 0);
+	root2 = insertOrUpdate(root2, NULL, _2nodeToInsert4, 0);
+	root2 = insertOrUpdate(root2, NULL, _2nodeToInsert5, 0);
 
 	
 	printf("\nroot %i, valor: %i", root2->key == 60, root2->key);
@@ -309,11 +319,11 @@ int main( void ) {
 	Node * _3nodeToInsert5 = getNode();
 	_3nodeToInsert5->key = 65;
 	
-	root3 = inserir(root3, NULL, _3nodeToInsert, 0);
-	root3 = inserir(root3, NULL, _3nodeToInsert2, 0);
-	root3 = inserir(root3, NULL, _3nodeToInsert3, 0);
-	root3 = inserir(root3, NULL, _3nodeToInsert4, 0);
-	root3 = inserir(root3, NULL, _3nodeToInsert5, 0);
+	root3 = insertOrUpdate(root3, NULL, _3nodeToInsert, 0);
+	root3 = insertOrUpdate(root3, NULL, _3nodeToInsert2, 0);
+	root3 = insertOrUpdate(root3, NULL, _3nodeToInsert3, 0);
+	root3 = insertOrUpdate(root3, NULL, _3nodeToInsert4, 0);
+	root3 = insertOrUpdate(root3, NULL, _3nodeToInsert5, 0);
 
 	
 	printf("\nroot %i, valor: %i", root3->key == 71, root3->key);
